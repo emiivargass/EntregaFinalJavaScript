4a297a3f7822d8820e87283cb55fee69f53217d5
Emiliano Vargas <emiivargass93@gmail.com>
2023-02-24 22:36:55 -0300

    DarkMode

--- app.js
+++ app.js
@@ -111,3 +111,27 @@ function game_over() {
 }
 
 game_over() ///desabilito los botones de las letras para iniciar el juego
+
+
+const toggle = document.querySelector('#toggle');
+const themeActual = localStorage.getItem('theme');
+
+if (themeActual) {
+  document.documentElement.setAttribute('data-theme', themeActual);
+}
+
+if (themeActual === 'oscuro') {
+  toggle.checked = true;
+}
+
+const cambiarTheme = (event) => {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'oscuro');
+    localStorage.setItem('theme', 'oscuro');
+  } else {
+    document.documentElement.setAttribute('data-theme', null);
+    localStorage.setItem('theme', null);
+  }
+};
+
+toggle.addEventListener('click', cambiarTheme);
